5e3e100871cd7eef7e074f8abd70dd008482de9a
ajayjoshioutdosolutions <ajay.joshi@outdosolutions.com>
2022-07-19 18:07:42 +0530

    task 2 completed

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,9 @@ Route::get('/', function () {
     // 3.1 Count of all active and verified users.
     $users = App\Models\User::Status('Active')->get();
 
+    // 3.2. Count of active and verified users who have attached active products.
+
+    $usersProductsAttached = App\Models\User::Status('Active')->whereHas('products')->get();
+
     return view('welcome',compact('users','usersProductsAttached'));
 });
